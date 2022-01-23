8266e127d3df449bd7d2fbbe51fdbc3c6a6e3cb0
surzia <songjie19970916@163.com>
2022-01-23 12:47:08 +0800

    Support command line input

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
+import (
+	"flag"
+)
+
+var word = flag.String("word", "中国", "The word needs to predict.")
+
 func main() {
 	lines := loadSoHuData("data/Sohu.dat")
 	ngram := NewNGram(3)
 	ngram.Init(lines)
 
-	ret := ngram.Predict("中国")
+	ret := ngram.Predict(*word)
 	for _, next := range ret {
 		next.Print()
 	}
